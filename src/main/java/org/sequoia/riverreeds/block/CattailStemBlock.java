ee39f48efe3f2d1044c49deff9baa1d8490ec46b
Jakob <jwaldh@outlook.com>
2025-09-02 15:12:54 +0200

    Added Mud as Block which cattails are able to be placed on

--- src/main/java/org/sequoia/riverreeds/block/CattailStemBlock.java
+++ src/main/java/org/sequoia/riverreeds/block/CattailStemBlock.java
@@ -8,7 +8,6 @@ import net.minecraft.item.ItemConvertible;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.BooleanProperty;
-import net.minecraft.state.property.IntProperty;
 import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.random.Random;
